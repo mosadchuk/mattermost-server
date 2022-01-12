ea03f88b5062a8b1a9c36cfb95f4c3f99928f7ba
maxim <maxim.osadchuk@gmail.com>
2022-01-12 12:17:06 +0100

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -20,7 +20,8 @@ echo "GOFLAGS: $GOFLAGS"
 find . -name 'cprofile*.out' -exec sh -c 'rm "{}"' \;
 find . -type d -name data -not -path './vendor/*' -not -path './data' | xargs rm -rf
 
-$GO test $GOFLAGS -run=$TESTS $TESTFLAGS -v -timeout=$TIMEOUT -covermode=$COVERMODE -coverpkg=$PACKAGES_COMMA -exec $DIR/test-xprog.sh $PACKAGES 2>&1 > >( tee output )
+$GO test $GOFLAGS -run=Bot $TESTFLAGS -v -timeout=$TIMEOUT -covermode=$COVERMODE -coverpkg=$PACKAGES_COMMA -exec $DIR/test-xprog.sh $PACKAGES 2>&1 > >( tee output )
+
 EXIT_STATUS=$?
 
 cat output | $GOBIN/go-junit-report > report.xml
